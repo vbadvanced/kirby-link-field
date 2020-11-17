6bd79df66b74791d9c2759beb8ff5d90e79f1234
Mohammad Shiran <vbadvanced@gmail.com>
2020-11-17 15:14:41 +0330

    Add Persian (Farsi) Translations

--- index.php
+++ index.php
@@ -171,7 +171,8 @@ App::plugin('oblik/link-field', [
     ],
     'translations' => [
         'en' => require_once __DIR__ . '/languages/en.php',
-        'de' => require_once __DIR__ . '/languages/de.php'
+        'de' => require_once __DIR__ . '/languages/de.php',
+        'fa' => require_once __DIR__ . '/languages/fa.php'
     ],
     'fieldMethods' => [
         'toLinkObject' => function ($field) {
---
+++ languages/fa.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'url' => 'آدرس',
+  'page' => 'صفحه',
+  'file' => 'فایل',
+  'email' => 'ایمیل',
+  'tel' => 'تلفن',
+  'label.text' => 'عنوان لینک',
+  'label.popup' => 'گشایش در صفحه جدید',
+  'label.fragment' => 'پارامتر',
+  'label.settings' => 'تنظیمات',
+  'label.close' => 'بازگشت',
+  'placeholder.elementid' => 'کد عنصر',
+  'error.type' => 'نوع فیلد نامعتبر است: {type}'
+];
